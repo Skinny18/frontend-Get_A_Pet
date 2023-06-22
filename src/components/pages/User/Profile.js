84caaadf28670be354d3bb419b4e72e1d4b465e9
Gabriel Carlos <gcgabriel257@gmail.com>
2023-06-22 19:15:19 -0400

    commit: profile

--- src/components/pages/User/Profile.js
+++ src/components/pages/User/Profile.js
@@ -1,28 +1,84 @@
 import React from 'react'
 
+import api from '../../../utils/api'
+
 import { useState, useEffect } from 'react'
+
+import useFlashMessage from '../../../hooks/useFlashMessage'
 
 import styles from './Profile.module.css'
 import formStyles from '../../form/Form.module.css'
 
 import Input from '../../form/Input'
+import RoundedImage from '../../layout/RoundedImage'
 
 const Profile = () => {
 
   const[user, setUser] = useState({})
+  const[preview, setPreview] = useState()
+  const [token] = useState(localStorage.getItem('token') || ' ')
+  const {setFlashMessage} = useFlashMessage()
+
+  useEffect(() => {
+    api.get('/users/checkuser', {
+      headers:{
+        Authorization: `Bearer ${JSON.parse(token)}`
+      }
+    }).then((response) => {
+      setUser(response.data)
+    })
+  }, [token])
 
   function onFileChange(e){
+    setPreview(e.target.files[0])
+    setUser({...user, [e.target.name]: e.target.files[0]})
 
   }
 
   function handleChange(e){
+    setUser({...user, [e.target.name]: e.target.value})
 
   }
+
+  async function handleSubmit(e){
+    e.preventDefault()
+
+    let msgType = 'success'
+
+    const formData = new FormData()
+
+    await Object.keys(user).forEach((key) => 
+      formData.append(key, user[key])
+    )
+
+    const data = await api.patch(`/users/edit/${user._id}`, formData, {
+      headers:{
+        Authorization: `Bearer ${JSON.parse(token)}`,
+        'Content-Type' : 'multipart/form-data'
+      }
+    }).then((response) => {
+      return response.data
+    }).catch((err) => {
+      msgType = 'error'
+      return err.response.data
+    })
+
+    setFlashMessage(data.message, msgType)
+
+  }
+
   return (
     <section>
-      <h1>Perfil</h1>
-      <p>Preview Image</p>
-      <form className={formStyles.form_container}>
+      <div className={styles.profile_container}>
+        <h1>Perfil</h1>
+        {(user.image || preview) && (
+          <RoundedImage src={preview ? URL.createObjectURL(preview) : `${process.env.REACT_APP_API}/images/users/${user.image}`}
+          alt={user.name}
+          />
+        )}
+      </div>
+      
+      <form onSubmit={handleSubmit} className={formStyles.form_container}>
         <Input
           text="Imagem"
           type="file"
